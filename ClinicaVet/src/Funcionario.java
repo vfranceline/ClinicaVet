29572a06bdc721878525ecf1b38f2a285e701fcc
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-10 17:30:47 -0300

    refactor: chamando atributos da classe Pessoa

--- ClinicaVet/src/Funcionario.java
+++ ClinicaVet/src/Funcionario.java
@@ -1,48 +1,9 @@
-public class Funcionario {
-    private String nome;
-    private String cpf;
-    private String email;
-    private String telefone;
-    private String turnoDeTrabalho;
+public class Funcionario extends Pessoa{
+    protect String turnoDeTrabalho;
 
     public Funcionario(String nome, String cpf, String email, String telefone, String turnoDeTrabalho) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.telefone = telefone;
+        super(nome, cpf, email, telefone);
         this.turnoDeTrabalho = turnoDeTrabalho;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
     }
 
     public String getTurnoDeTrabalho() {
