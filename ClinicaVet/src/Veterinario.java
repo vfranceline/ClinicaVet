73e1deda5141197e0cfd62284bbd8e5e229202aa
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-10 17:33:41 -0300

    refactor: chamando atributos da classe Pessoa

--- ClinicaVet/src/Veterinario.java
+++ ClinicaVet/src/Veterinario.java
@@ -1,51 +1,11 @@
 public class Veterinario {
-
-    private String nome;
-    private String cpf;
-    private String email;
-    private String telefone;
-    private String especialidade;
-    private String cfmv;
+    protect String especialidade;
+    protect String cfmv;
 
     public Veterinario(String nome, String cpf, String email, String telefone, String especialidade, String cfmv) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.telefone = telefone;
+        super(nome, cpf, email, telefone);
         this.especialidade = especialidade;
         this.cfmv = cfmv;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
     }
 
     public String getEspecialidade() {
