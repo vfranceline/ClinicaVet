596507b1a6c40280415936f995e5d5e7a0caf437
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-10 17:32:37 -0300

    refactor: chamando atributos da classe Pessoa

--- ClinicaVet/src/Tutor.java
+++ ClinicaVet/src/Tutor.java
@@ -1,50 +1,11 @@
 public class Tutor {
-    private String nome;
-    private String cpf;
-    private String email;
-    private String telefone;
-    private String endereco;
+    protect String endereco;
 
     public Tutor(String nome, String cpf, String email, String telefone, String endereco) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.telefone = telefone;
+        super(nome, cpf, email, telefone);
         this.endereco = endereco;
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
+    
     public String getEndereco() {
         return endereco;
     }
