162e35f55fd2839bd4a7438327a20a257477e6aa
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-12 17:14:23 -0300

    feat: animal a ser atendido

--- ClinicaVet/src/Agendamento.java
+++ ClinicaVet/src/Agendamento.java
@@ -2,11 +2,13 @@ public class Agendamento {
     private Data dataConsulta;
     private Horario hora;
     private String especialidade;
+    private Animal animal;
 
     public Agendamento(Data dataConsulta, Horario hora, String especialidade) {
         this.dataConsulta = dataConsulta;
         this.hora = hora;
         this.especialidade = especialidade;
+        this.animal = animal;
     }
 
     public Data getDataConsulta() {
@@ -31,5 +33,13 @@ public class Agendamento {
 
     public void setEspecialidade(String especialidade) {
         this.especialidade = especialidade;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
     }
 }
