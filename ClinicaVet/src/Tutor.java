c7aee30e1a32e9cb85119822b7950cb4748bba0b
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-05-12 17:04:49 -0300

    fix: protected, feat: arrarlist animais

--- ClinicaVet/src/Tutor.java
+++ ClinicaVet/src/Tutor.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+private ArrayList<Animal> animais;
+
 public class Tutor extends Pessoa{
-    protect String endereco;
+    protected String endereco;
 
     public Tutor(String nome, String cpf, String email, String telefone, String endereco) {
         super(nome, cpf, email, telefone);
         this.endereco = endereco;
+        this.animais = new ArrayList<>();
     }
     
     public String getEndereco() {
@@ -12,5 +16,17 @@ public class Tutor extends Pessoa{
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
+    }
+
+    public ArrayList<Animal> getAnimais() {
+        return animais;
+    }
+
+    public void addAnimal(Animal animal) {
+        animais.add(animal);
+    }
+
+    public void removerAnimal(Animal animal) {
+        animais.remove(animal);
     }
 }
